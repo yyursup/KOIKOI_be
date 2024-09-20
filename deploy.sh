4b763d873e10925997bc1d588f4207f824c01bd7
Minhnetroi <vanminh290204@gmail.com>
2024-09-21 00:37:53 +0700

    đã sửa lại code ở deploy.sh target/be.jar thành target/SWP-0.0.1-SNAPSHOT.jar và -jar be.jar thành -jar SWP-0.0.1-SNAPSHOT.jar

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ echo "Building app..."
 ./mvnw clean package
 
 echo "Deploy files to server..."
-scp -r  target/be.jar root@14.225.207.153:/var/www/be/
+scp -r  target/SWP-0.0.1-SNAPSHOT.jar root@14.225.207.153:/var/www/be/
 
 ssh root@14.225.207.153 <<EOF
 pid=\$(sudo lsof -t -i:8081)
@@ -14,7 +14,7 @@ else
     sudo kill -9 "\$pid"
 fi
 cd /var/www/be
-java -jar be.jar
+java -jar SWP-0.0.1-SNAPSHOT.jar
 EOF
 exit
 echo "Done!"
