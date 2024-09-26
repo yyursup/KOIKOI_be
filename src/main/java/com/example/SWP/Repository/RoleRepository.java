780756f4e3be77961cf2d050aafcf46011dc4209
Minhnetroi <vanminh290204@gmail.com>
2024-09-26 16:33:42 +0700

    Toi da viet them token cho bai

--- src/main/java/com/example/SWP/Repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SWP.Repository;
-
-import com.example.SWP.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-    Role findRoleById(long id);
-}
--- src/main/java/com/example/SWP/entity/Role.java
+++ src/main/java/com/example/SWP/entity/Role.java
@@ -1,26 +1,8 @@
 package com.example.SWP.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Entity
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Role {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
-
-    boolean isDeleted = false;
-    String roleName;
-
-    int roleType;
+public enum Role {
+    MANAGER,
+    STAFF,
+    GUEST,
+    CUSTOMER
 }
