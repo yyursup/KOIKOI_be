74cdb66a8fac0e0d9fdd5efaafcc55efbc15b03b
Minhne <minhnvse182350@fpt.edu.vn>
2024-11-12 18:29:39 +0700

    update Consignment

--- src/main/java/com/example/SWP/Repository/OrderRepository.java
+++ src/main/java/com/example/SWP/Repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package com.example.SWP.Repository;
 
+import com.example.SWP.Enums.Type;
 import com.example.SWP.entity.KoiOrder;
 import com.example.SWP.Enums.OrderStatus;
 import jakarta.persistence.criteria.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -16,12 +18,13 @@ public interface OrderRepository extends JpaRepository<KoiOrder, Long> {
     @Query("SELECT o FROM KoiOrder o where o.account.id = :accountId ")
     List<KoiOrder> findAllOrdersByAccountId(@Param("accountId") long accountId);
 
-    List<KoiOrder> findByOrderStatus(OrderStatus orderStatus);
+    List<KoiOrder> findByOrderStatusAndType(OrderStatus orderStatus, Type type);
 
     KoiOrder findKoiOrderById(long id);
 
     List<KoiOrder> findAllByOrderStatusAndProcessingDateLessThan(OrderStatus orderStatus, Date processingDate);
 
+    List<KoiOrder> findKoiOrderByType(Type type);
 
 
 }
